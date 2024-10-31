7b314ee69658d78d51d377a9586317d0f64eb629
renan234R <106178709+renan234R@users.noreply.github.com>
2024-10-31 11:35:14 -0300

    Add files via upload

--- common.js
+++ common.js
@@ -1,11 +1,30 @@
 const getCSS = (variavel) => {
     return getComputedStyle(document.body).getPropertyValue(variavel)
-    }
-    
-    const tickConfig = {
+}
 
+const tickConfig = {
     color: getCSS('--primary-color'),
-  size: 16,
-  family: getCSS('--font')
+    size: 16,
+    family: getCSS('--font')
+}
+
+function criarGrafico(data, layout) {
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    const config = {
+        responsive: true,
+        displayModeBar: false
     }
-    export {getCSS, tickConfig }
+    Plotly.newPlot(grafico, data, layout, config)
+}
+
+function incluirTexto(texto) {
+    const container = document.getElementById('graficos-container')
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = texto
+    container.appendChild(paragrafo)
+}
+
+export { getCSS, tickConfig, criarGrafico, incluirTexto }
--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -1,36 +1,37 @@
-import { getCSS, tickConfig } from "./common.js"
-async function quantidadeUsuarios() {
+import { getCSS, tickConfig, criarGrafico } from "./common.js"
+
+async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
-    const quantidadeUsuarios = Object.values(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
     const data = [
-
         {
-        x: nomeDasRedes,
-        y: quantidadeUsuarios,
-        type: 'bar',
-        marker: {
-        color: getCSS('--primary-color')
-               
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
         }
-        }
-        ]
+    ]
 
-        const layout = {
-            plot_bgcolor: getCSS('--bg-color'),
-            paper_bgcolor: getCSS('--bg-color'),
-            title: {
-              text: 'Redes sociais com mais usuários no mundo',
-              x: 0,
-              font: {
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Redes sociais com mais usuários no mundo',
+            x: 0,
+            font: {
                 color: getCSS('--primary-color'),
                 family: getCSS('--font'),
-             size: 30
+                size: 30
             }
-          },
-          xaxis: {
+        },
+        xaxis: {
+            tickfont: tickConfig,
             title: {
                 text: 'nome das redes sociais',
                 font: {
@@ -39,6 +40,7 @@ async function quantidadeUsuarios() {
             }
         },
         yaxis: {
+            tickfont: tickConfig,
             title: {
                 text: 'bilhões de usuários ativos',
                 font: {
@@ -46,10 +48,10 @@ async function quantidadeUsuarios() {
                 }
             }
         }
-        }
-    const grafico = document.createElement ('div')
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data, layout)
+
     }
-    quantidadeUsuarios()
+
+    criarGrafico(data, layout)
+}
+
+quantidadeUsuariosPorRede()
